cfafa4578c7eeb47b1fdb796583f8980a757102a
Mirinae <137975323+mirinae123@users.noreply.github.com>
2025-07-10 02:31:19 +0900

    Issue: #17 CameraManager 클래스 정리

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -8,31 +8,34 @@ using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 
 /// <summary>
-/// 카메라를 관리하는 클래스 (임시용)
+/// 카메라를 관리하는 클래스
 /// </summary>
-public class CameraManager : MonoBehaviour
+public class CameraManager : SingletonBehaviour<CameraManager>
 {
     [SerializeField]
-    private float r, pi;
-    private float x, z, phi = Mathf.PI / 4;
-    private float h, v, x_mov, z_mov;
-
-    private float[] rotQuant = { 45f, 135f, 225f, 315f };
-    private int currentRot, nextRot;
-    private float rotDuration = .5f;
-    private bool isRotating;
+    private float _r, _pi;
 
     [SerializeField]
-    private float movSpeed, rotSpeed;
+    private float _moveSpeed, _rotationSpeed;
 
     [SerializeField]
-    private float maxZoom, minZoom;
-    Camera cam;
-    private float zoom;
+    private float _maxZoom, _minZoom;
+
+    private float _x, _z, _phi = Mathf.PI / 4;
+    private float _h, _v, _deltaX, _deltaZ;
+
+    private float[] _rotationValues = { 45f, 135f, 225f, 315f };
+    private float _rotationDuration = .5f;
+
+    private Camera _camera;
+
+    private int _currentRotation, _nextRotation;
+    private bool _isRotating;
+    private float _currentZoom; 
 
     private void Start()
     {
-        cam = GetComponent<Camera>();
+        _camera = GetComponent<Camera>();
 
         InputHandler.Instance.OnMoveInput += OnMoveInput;
         InputHandler.Instance.OnRotateInput += OnRotateInput;
@@ -40,18 +43,20 @@ public class CameraManager : MonoBehaviour
 
     private void Update()
     {
-        if (!isRotating)
+        if (!_isRotating)
         {
-            x += x_mov * movSpeed * Time.deltaTime * Mathf.Sqrt(zoom);
-            z += z_mov * movSpeed * Time.deltaTime * Mathf.Sqrt(zoom);
+            _x += _deltaX * _moveSpeed * Time.deltaTime * Mathf.Sqrt(_currentZoom);
+            _z += _deltaZ * _moveSpeed * Time.deltaTime * Mathf.Sqrt(_currentZoom);
         }
 
-        zoom -= Input.mouseScrollDelta.y * 3;
-        zoom = Mathf.Clamp(zoom, minZoom, maxZoom);
-        cam.orthographicSize = zoom;
+        _deltaX = _v * Mathf.Cos(_phi) + _h * Mathf.Cos(_phi - Mathf.PI / 2.0f);
+        _deltaZ = _v * Mathf.Sin(_phi) + _h * Mathf.Sin(_phi - Mathf.PI / 2.0f);
 
-        transform.position = new Vector3(x + r * Mathf.Cos(phi), -r * Mathf.Sin(pi), z + r * Mathf.Sin(phi));
-        transform.LookAt(new Vector3(x, 0, z));
+        _currentZoom = Mathf.Clamp(_currentZoom - Input.mouseScrollDelta.y * 3, _minZoom, _maxZoom);
+        _camera.orthographicSize = _currentZoom;
+
+        transform.position = new Vector3(_x + _r * Mathf.Cos(_phi), -_r * Mathf.Sin(_pi), _z + _r * Mathf.Sin(_phi));
+        transform.LookAt(new Vector3(_x, 0, _z));
     }
 
     private void OnDestroy()
@@ -66,23 +71,21 @@ public class CameraManager : MonoBehaviour
     private void OnMoveInput(InputValue value)
     {
         Vector2 input = value.Get<Vector2>();
-        h = -input[0];
-        v = -input[1];
 
-        x_mov = v * Mathf.Cos(phi) + h * Mathf.Cos(phi - Mathf.PI / 2.0f);
-        z_mov = v * Mathf.Sin(phi) + h * Mathf.Sin(phi - Mathf.PI / 2.0f);
+        _h = -input[0];
+        _v = -input[1];
     }
 
     private void OnRotateInput(InputValue value)
     {
         float input = value.Get<float>();
 
-        if (!isRotating && input != 0)
+        if (!_isRotating && input != 0)
         {
-            isRotating = true;
+            _isRotating = true;
 
-            if (input > 0) nextRot = (currentRot + 1) % rotQuant.Length;
-            else nextRot = currentRot == 0 ? 3 : currentRot - 1;
+            if (input > 0) _nextRotation = (_currentRotation + 1) % _rotationValues.Length;
+            else _nextRotation = _currentRotation == 0 ? 3 : _currentRotation - 1;
 
             StartCoroutine("Rotate");
         }
@@ -90,16 +93,17 @@ public class CameraManager : MonoBehaviour
 
     private IEnumerator Rotate()
     {
-        float duration = 0f;
+        float elapsed = 0f;
 
-        while (duration < rotDuration)
+        while (elapsed < _rotationDuration)
         {
-            phi = Mathf.LerpAngle(rotQuant[currentRot], rotQuant[nextRot], duration / rotDuration) * Mathf.Deg2Rad;
-            duration += 0.01f;
-            yield return new WaitForSeconds(0.01f);
+            _phi = Mathf.LerpAngle(_rotationValues[_currentRotation], _rotationValues[_nextRotation], elapsed / _rotationDuration) * Mathf.Deg2Rad;
+            elapsed += Time.deltaTime;
+
+            yield return null;
         }
 
-        currentRot = nextRot;
-        isRotating = false;
+        _currentRotation = _nextRotation;
+        _isRotating = false;
     }
 }
