6035cd7f32e4bcc57acb2637916df1b29fef92fe
Mirinae <137975323+mirinae123@users.noreply.github.com>
2025-08-19 14:23:22 +0900

    Issue: #26 타이틀 씬에 기본 내용물 추가

---
+++ Assets/Scripts/UI/TitleUI.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+/// <summary>
+/// 타이틀 메뉴 클래스
+/// </summary>
+public class TitleUI : MonoBehaviour
+{
+    [SerializeField] Button _newGameButton;
+    [SerializeField] Button _loadButton;
+    [SerializeField] Button _recordButton;
+    [SerializeField] Button _optionButton;
+    [SerializeField] Button _quitButton;
+
+    void Start()
+    {
+        _newGameButton.onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene(1);
+        });
+
+        _loadButton.onClick.AddListener(() =>
+        {
+        });
+
+        _recordButton.onClick.AddListener(() =>
+        {
+        });
+
+        _optionButton.onClick.AddListener(() =>
+        {
+            UIManager.Instance.ShowPanel(PanelType.Option);
+        });
+
+        _quitButton.onClick.AddListener(() =>
+        {
+        });
+    }
+}
